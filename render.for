99e7895ef9fdf907b38226ed4bee1f01782577fe
Jason Stevens <jason.r.stevens@live.com>
2026-04-23 22:27:31 +0100

    fixes from chatgpt

--- render.for
+++ render.for
@@ -1,18 +1,53 @@
+C       ray trace port from basic      
+C       really sad attempot
+C       chatgpt fixed it for me becasue im dum
+
+      INCLUDE  'FGRAPH.FI'
+      INCLUDE  'FGRAPH.FD'
+      
       INTEGER M,N,GC,COLS
       INTEGER MAXX,MAXY
       INTEGER x1,x2,xx
       REAL U,V,W
       REAL X,Y,Z
       REAL E,F,G
-      COMPLEX P
+      REAL P
       REAL I
       REAL D,T
       REAL POWER
       INTEGER LOOKUP(16)
+      INTEGER RC
+
+C        PARAMETER($MAXRESMODE   =-3)    ! graphics mode with highest resolution
+C        PARAMETER($MAXCOLORMODE =-2)    ! graphics mode with most colors
+C        PARAMETER($DEFAULTMODE  =-1)    ! restore screen to original mode
+C        PARAMETER($TEXTBW40      =0)    ! 40 x 25 text, 16 grey
+C        PARAMETER($TEXTC40       =1)    ! 40 x 25 text, 16/8 color
+C        PARAMETER($TEXTBW80      =2)    ! 80 x 25 text, 16 grey
+C        PARAMETER($TEXTC80       =3)    ! 80 x 25 text, 16/8 color
+C        PARAMETER($MRES4COLOR    =4)    ! 320 x 200, 4 color
+C        PARAMETER($MRESNOCOLOR   =5)    ! 320 x 200, 4 grey
+C        PARAMETER($HRESBW        =6)    ! 640 x 200, BW
+C        PARAMETER($TEXTMONO      =7)    ! 80 x 25 text, BW
+C        PARAMETER($HERCMONO      =8)    ! 720 x 348, BW for HGC
+C        PARAMETER($MRES16COLOR   =13)   ! 320 x 200, 16 color
+C        PARAMETER($HRES16COLOR   =14)   ! 640 x 200, 16 color
+C        PARAMETER($ERESNOCOLOR   =15)   ! 640 x 350, BW
+C        PARAMETER($ERESCOLOR     =16)   ! 640 x 350, 4 or 16 color
+C        PARAMETER($VRES2COLOR    =17)   ! 640 x 480, BW
+C        PARAMETER($VRES16COLOR   =18)   ! 640 x 480, 16 color
+C        PARAMETER($MRES256COLOR  =19)   ! 320 x 200, 256 color
+C        PARAMETER($ORESCOLOR     =64)   ! 640 x 400, 1 of 16 colors (Olivetti)
+
       
-      COLS = 2
+      MODE = $MRES4COLOR
+      COLS = 4
       MAXX = 320
       MAXY = 200
+      SCRY = MAXY-1
+      IF( setvideomode( MODE ) .EQ. 0 )
+     +    STOP 'Error:  cannot set VGA graphics mode'
+
 
       DATA LOOKUP /0,8,2,10,12,4,14,6,3,11,1,9,15,7,13,5/
 
@@ -24,16 +59,16 @@
       Z = 3
 
 C     -1 to 1 scaled position X
-      U=(M-159.2)
+      U=(M-159.2) / 160
 C     -1 to 1 scaled position Y
-      V=(N-127.5)
+      V=(N-127.5) / 160
 C     Reciprocal distance from centre.
       W = 1 / SQRT(U * U + V * V + 1)
 C     REM Normalise U and V
       U = U * W
       V = V * W
 C     left or right half of screen
-      I = SIGN(1,U)
+      I = SIGN(1.0, U)
       G = 1
 
 C     Walk towards the opposite edge.
@@ -66,13 +101,17 @@ C     Ground Checkerboard
 C     V = -V * (MOD(INT(X - U * P) + INT(Z - W * P), 2)) / 2.0+0.3+0.2
       x1 = INT(X - U * P)
       x2 = INT(Z - W * P)
-      xx = INT(x1 + x2 .AND. 1)
+      xx = IAND(x1 + x2, 1)
       V = -V * (xx / 2.0+0.3) +0.2
 C     Select the dithering pattern in the lookup table
 450   POWER = V ** 0.4
       GC = ABS((COLS - 1) - (INT(((COLS - 1) * 16) * POWER + 
-     1    LOOKUP(MOD(M,4) + (MOD(N,4) * 4))) / 16))
-      PRINT *, "X:", M,"Y:", N, "D:", GC
+C     1    LOOKUP(MOD(M,4) + (MOD(N,4) * 4))/3)+1 / 16))
+     1    LOOKUP((MOD(M,4) + (MOD(N,4) * 4))+1)/3) / 16))
+      RC = SETCOLOR(GC)
+      RC = setpixel(M,SCRY-N)
 20    CONTINUE
 10    CONTINUE
+      PAUSE
+      rc = setvideomode( $DEFAULTMODE )
       END
