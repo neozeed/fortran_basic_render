5359d418467d8b6958d146a7874d548c662e0ff1
Jason Stevens <jason.r.stevens@live.com>
2026-04-23 22:27:10 +0100

    frantic changes

--- render.for
+++ render.for
@@ -1,58 +1,78 @@
-      integer m,n,gc,cols
-      integer maxx,maxy
-      real u,v,w
-      real x,y,z
-      real e,f,g
-      complex p
-      real i
-      real d,t
-      real power
-      INTEGER lookup(16)
+      INTEGER M,N,GC,COLS
+      INTEGER MAXX,MAXY
+      INTEGER x1,x2,xx
+      REAL U,V,W
+      REAL X,Y,Z
+      REAL E,F,G
+      COMPLEX P
+      REAL I
+      REAL D,T
+      REAL POWER
+      INTEGER LOOKUP(16)
       
-      cols = 16
-      maxx = 320
-      maxy = 200
+      COLS = 2
+      MAXX = 320
+      MAXY = 200
 
-      DATA lookup /0,8,2,10,12,4,14,6,3,11,1,9,15,7,13,5/
+      DATA LOOKUP /0,8,2,10,12,4,14,6,3,11,1,9,15,7,13,5/
 
-      do 10 n = 0,maxy-1
-      do 20 m = 0,maxx-1
+      DO 10 N = 0,MAXY-1
+      DO 20 M = 0,MAXX-1
      
-      x = 0
-      y = -1
-      z = 3
-      u=(m-159.2)
-      v=(n-127.5)
-      w = 1 / SQRT(u * u + v * v + 1)
+      X = 0
+      Y = -.1
+      Z = 3
+
+C     -1 to 1 scaled position X
+      U=(M-159.2)
+C     -1 to 1 scaled position Y
+      V=(N-127.5)
+C     Reciprocal distance from centre.
+      W = 1 / SQRT(U * U + V * V + 1)
+C     REM Normalise U and V
       U = U * W
       V = V * W
-      i = SIGN(1,U)
-      g = 1
-230   E = X - i
-      F = Y - i
-      P = U * E + V * F - W * z
-      D = P * P - E * E - F * F - z * z + 1
+C     left or right half of screen
+      I = SIGN(1,U)
+      G = 1
+
+C     Walk towards the opposite edge.
+230   E = X - I
+C     Walk towards the top/bottom.
+      F = Y - I
+      P = U * E + V * F - W * Z
+
+      D = P * P - E * E - F * F - Z * Z + 1
+
       IF (D .LE. 0) GOTO 420
       T = -P - SQRT(D)
       IF (T .LE. 0) GOTO 420
       X = X + T * U
       Y = Y + T * V
-      z = z - T * W
-      E = X - i
-      F = Y - i
-      g = z
-      P = 2 * (U * E + V * F - W * g)
+      Z = Z - T * W
+      E = X - I
+      F = Y - I
+      G = Z
+      P = 2 * (U * E + V * F - W * G)
       U = U - P * E
       V = V - P * F
-      W = W + P * g
-      i = -i
-      goto 230
-420   P = (Y + 2) / V
-      V = -V * (MOD(INT(X - U * P) + INT(Z - W * P), 2)) / 2.0+0.3+0.2
-      power = V ** 0.4
-      gc = ABS((cols - 1) - (INT(((cols - 1) * 16) * power + 
-     1lookup(MOD(m,4) + 1 + (MOD(n,4) * 4))) / 16))
-      print *, "Integers:", m, n, "Real:", gc
-20    continue
-10    continue
+      W = W + P * G
+      I = -I
+      GOTO 230
+
+C     Ground Checkerboard
+420   IF (V .GT.0) GOTO 450
+      P = (Y + 2) / V
+C     V = -V * (MOD(INT(X - U * P) + INT(Z - W * P), 2)) / 2.0+0.3+0.2
+      x1 = INT(X - U * P)
+      x2 = INT(Z - W * P)
+      xx = INT(x1 + x2 .AND. 1)
+      V = -V * (xx / 2.0+0.3) +0.2
+C     Select the dithering pattern in the lookup table
+450   POWER = V ** 0.4
+      GC = ABS((COLS - 1) - (INT(((COLS - 1) * 16) * POWER + 
+     1    LOOKUP(MOD(M,4) + (MOD(N,4) * 4))) / 16))
+      PRINT *, "X:", M,"Y:", N, "D:", GC
+20    CONTINUE
+10    CONTINUE
       END
